06da971913fd5a7d8f39af6e1d5387b1973166de
vasin yeagorr <anyaivantsova22@gmail.com>
2023-06-14 15:30:08 +0300

    Revert "Similar developer search (low connectivity approach)"
    This reverts commit f4154041c935f3c356b8cc74ad4837ae53d46d29.

--- src/model/constants.py
+++
@@ -1,9 +0,0 @@
-headers = {'Authorization': 'token '}
-
-token = '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'  # input('Github token: ')
-headers['Authorization'] = 'token ' + token
-stargazer_pages_num = 2
-repos_pages_num = 2
-
-repos_limit = 4
-max_candidates_num = 100
