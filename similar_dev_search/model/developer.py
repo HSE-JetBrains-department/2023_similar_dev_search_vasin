bb49c08c44f0e1461d592f200b5082d32f6627cb
vasin yeagorr <anyaivantsova22@gmail.com>
2023-06-19 17:44:15 +0300

    Extract variables
    Parse ast from code, extract variables and make methods in developer and repo class

--- similar_dev_search/model/developer.py
+++ similar_dev_search/model/developer.py
@@ -1,14 +1,20 @@
-from typing import List
+from collections import defaultdict
+from typing import List, Tuple
 
 import httpx
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 import model.fetcher as fetcher
-from model.constants import headers
+from model.constants import headers, repos_limit
 from model.repository import Repository
 
 
 class Developer:
     def __init__(self, url: str):
+        self.languages = None
+        self.variables = None
+        self.repos = None
         self.url = url
         url = url.replace('https://', '')
         url_prefix, id = url.split('/')
@@ -33,9 +39,50 @@ class Developer:
 
         return starred_repos
 
+    async def get_languages(self, asyncio_client: httpx.AsyncClient = None) -> defaultdict[int]:
+        """
+        Gets dict of languages used by the developer
+        :param asyncio_client: asyncio client to perform requests from
+        :return: dict with language key and number of occurrences value
+        """
+        if self.languages is not None:
+            return self.languages
+        languages, _ = await self.get_languages_variables(asyncio_client)
+        return languages
+
+    async def get_variables(self, asyncio_client: httpx.AsyncClient = None) -> defaultdict[int]:
+        """
+        Gets dict of variables used by the developer
+        :param asyncio_client:  asyncio client to perform requests from
+        :return: dict with variable key and number of occurrences value
+        """
+        if self.variables is not None:
+            return self.variables
+        _, variables = await self.get_languages_variables(asyncio_client)
+        return variables
+
+    async def get_languages_variables(self, asyncio_client: httpx.AsyncClient = None) \
+            -> Tuple[defaultdict[int], defaultdict[int]]:
+        """
+        Gets both languages and variables that developer uses
+        :param asyncio_client:  asyncio client to perform requests from
+        :return: dict of languages, dict of variables
+        """
+        if self.languages is not None and self.variables is not None:
+            return self.languages, self.variables
+        self.languages = defaultdict(int)
+        self.variables = defaultdict(int)
+        repos = await self.get_repos()
+
+        for i in range(min(repos_limit, len(repos))):
+            for language in (await repos[i].get_languages(asyncio_client)).keys():
+                self.languages[language] += (await repos[i].get_languages(asyncio_client))[language]
+            for variable in (await repos[i].get_variables(asyncio_client)).keys():
+                self.languages[variable] += (await repos[i].get_variables(asyncio_client))[variable]
+        return self.languages, self.variables
+
     def __str__(self) -> str:
         return self.url
-
 
     async def get_repos(self, asyncio_client: httpx.AsyncClient = None) -> List[Repository]:
         """
--- similar_dev_search/model/repository.py
+++ similar_dev_search/model/repository.py
@@ -1,4 +1,11 @@
+import asyncio
+from collections import defaultdict
+
+import httpx
+
 import model.fetcher as fetcher
+from model.constants import headers
+from model.language_extractor import fetch_language_variables
 
 
 class Repository:
@@ -18,3 +25,70 @@ class Repository:
 
     def __str__(self):
         return self.url
+
+    async def get_languages(self, asyncio_client: httpx.AsyncClient = None) -> defaultdict[int]:
+        """
+        Returns dict of languages for this repo
+        :param asyncio_client: asyncio client to perform requests from
+        :return: dict {language: occurrences}
+        """
+        if self.languages is not None:
+            return self.languages
+        languages, _ = await self.get_languages_variables(asyncio_client)
+        return languages
+
+    async def get_variables(self, asyncio_client: httpx.AsyncClient = None) -> defaultdict[int]:
+        """
+        Returns dict of variables for this repo
+        :param asyncio_client: asyncio client to perform requests from
+        :return: dict {variable: occurrences}
+        """
+        if self.variables is not None:
+            return self.variables
+        _, variables = await self.get_languages_variables(asyncio_client)
+        return variables
+
+    async def get_languages_variables(self, asyncio_client: httpx.AsyncClient = None):
+        """
+        Returns dict of languages and dict of variables
+        :param asyncio_client: asyncio client to perform requests from
+        :return: dict of languages, dict of variables
+        """
+        if self.languages is not None and self.variables is not None:
+            return self.languages, self.variables
+
+        self.languages = defaultdict(int)
+        self.variables = defaultdict(int)
+
+        if asyncio_client is None:
+            client = httpx.AsyncClient(timeout=None)
+        else:
+            client = asyncio_client
+
+        files_url = f"https://api.github.com/repos/{self.dev_id}/{self.repo_name}/contents"
+        response = await client.get(files_url, headers=headers)
+        files_data = response.json()
+
+        tasks = []
+        for file in files_data:
+            if file is str:
+                continue
+            if "type" not in file:
+                continue
+            if file["type"] == "file":
+                file_path = file["path"]
+                file_url = f"https://raw.githubusercontent.com/{self.dev_id}/{self.repo_name}/master/{file_path}"
+
+                tasks.append(fetch_language_variables(file_url, file_path[file_path.find('/') + 1:], client))
+
+        result = await asyncio.gather(*tasks)
+
+        if asyncio_client is None:
+            await client.aclose()
+        for language, variables in result:
+            if language == 'Ignore List' or language == '':
+                continue
+            self.languages[language] += 1
+            for var in variables.keys():
+                self.variables[var] += variables[var]
+        return self.languages, self.variables
